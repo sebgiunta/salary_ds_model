a9fbaea51cb3482f121eb91d2e8915a29d0fd31a
Mek900 <87013481+Mek900@users.noreply.github.com>
2022-06-04 15:55:06 +1000

    minor update

--- data_collection.py
+++ data_collection.py
@@ -22,3 +22,14 @@ df.to_csv('collected_data_v1.csv')
 df = gs.get_jobs(1930, False)
 
 df.to_csv('collected_data_v2.csv')
+
+
+# Merge 
+
+df_1 = pd.read_csv('collected_data_v1.csv')
+
+df_2 = pd.read_csv('collected_data_v2.csv')
+
+df_concat = pd.concat([df_1, df_2])
+
+df_concat.to_csv('collected_data_merged.csv', index=False)
