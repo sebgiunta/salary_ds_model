6577340392b48fad4121953ed703e145b7ac22e3
Mek900 <87013481+Mek900@users.noreply.github.com>
2022-06-21 09:10:33 +1000

    added images

--- model_building.py
+++ model_building.py
@@ -95,3 +95,17 @@ mean_absolute_error(y_test, tpred_lm)
 mean_absolute_error(y_test, tpred_lml)
 mean_absolute_error(y_test, tpred_rf)
 
+
+import pickle
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
+
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+    
+    
+model.predict(X_test.iloc[1,:].values.reshape(1,-1))
+
+list(X_test.iloc[1,:])
